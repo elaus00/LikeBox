cdccab3adae2148648cce84d8b068cfdbec6feaf
jihwannnn <kimjih1130@gmail.com>
2024-11-17 07:51:06 +0900

    Fix: fromMusicPlatform Id 다시 적용

--- app/src/main/java/com/example/likebox/domain/model/MusicPlatform.kt
+++ app/src/main/java/com/example/likebox/domain/model/MusicPlatform.kt
@@ -3,4 +3,8 @@ package com.example.likebox.domain.model
 enum class MusicPlatform {
     SPOTIFY,
     APPLE_MUSIC;
+
+    companion object {
+        fun fromId(id: String): MusicPlatform = valueOf(id.uppercase())
+    }
 }
